8d7440648e49011c216f34084c9384715b52b94f
SuperK2-OS <67206656+SuperK2-OS@users.noreply.github.com>
2020-07-16 14:35:14 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -48,20 +48,20 @@ void Backup()
 
 int main()
 {
-	SetConsoleTitle(" Inyector VACa v.2.1 - www.CheatersUnidos.tk - 16 - Julio - 2020 :)");
+	SetConsoleTitle(" Inyector VACa v.2.2 - www.CheatersUnidos.tk - 16 - Julio - 2020 :)");
 
-	cout << "888     888     d8888  .d8888b.                 .d8888b.       d888  " << endl;
-	cout << "888     888    d88888 d88P  Y88b               d88P  Y88b     d8888   " << endl;
-	cout << "888     888   d88P888 888    888                      888       888  " << endl;
-	cout << "Y88b   d88P  d88P 888 888         8888b.            .d88P       888  " << endl;
-	cout << " Y88b d88P  d88P  888 888            88b.        od888P         888  " << endl;
-	cout << "  Y88o88P  d88P   888 888    888 .d888888      d88P             888  " << endl;
+	cout << "888     888     d8888  .d8888b.                 .d8888b.         .d8888b.  " << endl;
+	cout << "888     888    d88888 d88P  Y88b               d88P  Y88b       d88P  Y88b   " << endl;
+	cout << "888     888   d88P888 888    888                      888              888  " << endl;
+	cout << "Y88b   d88P  d88P 888 888         8888b.            .d88P            .d88P  " << endl;
+	cout << " Y88b d88P  d88P  888 888            88b.        od888P           od888P  " << endl;
+	cout << "  Y88o88P  d88P   888 888    888 .d888888      d88P             d88P  " << endl;
 	cout << "   Y888P  d8888888888 Y88b   d88 888  888      888        d8b   888  " << endl;
-	cout << "    Y8P  d88P     888   Y8888P   Y888888.      888888888  Y8P 8888888\n" << endl;
+	cout << "    Y8P  d88P     888   Y8888P   Y888888.      888888888  Y8P   888888888\n" << endl;
 
 
 	cout << "*****************************************************************" << endl;
-	cout << "*  Inyector VACa 2.1 - www.CheatersUnidos.tk - 16 - Julio - 2020 *" << endl;
+	cout << "*  Inyector VACa 2.2 - www.CheatersUnidos.tk - 16 - Julio - 2020 *" << endl;
 	cout << "*****************************************************************\n\n" << endl;
 
 
