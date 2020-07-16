7574f790b62b64316bb9baf06fee57cd62ba86ba
SuperK2-OS <67206656+SuperK2-OS@users.noreply.github.com>
2020-07-16 14:28:51 +0200

    Actualización 2.2
    Cambios varios.

--- main.cpp
+++ main.cpp
@@ -24,7 +24,6 @@ void bypass()
 	}
 	else
 	{
-
 		cout << "Imposible Hacer Bypass :(\n";
 		Sleep(2000);
 		exit(-1);
@@ -41,27 +40,37 @@ void Backup()
 	}
 	else
 	{
-		cout << "Unable to backup :(\n";
+		cout << "NO SE PUEDE HACER BACKUP :(\n";
 		Sleep(2000);
 		exit(-1);
-
 	}
 }
 
 int main()
 {
-	SetConsoleTitle("VACa Inyector v.1 - www.CheatersUnidos.tk - 9 - Julio - 2020 :)");
-	
-	cout << "****************************************************************" << endl;
-	cout << "* VACa Inyector v.1 - www.CheatersUnidos.tk - 9 - Julio - 2020 *" << endl;
-	cout << "****************************************************************\n\n" << endl;
-	
-	
-	cout << "Creadores:\n Daniel Krupinski, online-9, Hitchance\n\n" << endl;
+	SetConsoleTitle(" Inyector VACa v.2.1 - www.CheatersUnidos.tk - 16 - Julio - 2020 :)");
+
+	cout << "888     888     d8888  .d8888b.                 .d8888b.       d888  " << endl;
+	cout << "888     888    d88888 d88P  Y88b               d88P  Y88b     d8888   " << endl;
+	cout << "888     888   d88P888 888    888                      888       888  " << endl;
+	cout << "Y88b   d88P  d88P 888 888         8888b.            .d88P       888  " << endl;
+	cout << " Y88b d88P  d88P  888 888            88b.        od888P         888  " << endl;
+	cout << "  Y88o88P  d88P   888 888    888 .d888888      d88P             888  " << endl;
+	cout << "   Y888P  d8888888888 Y88b   d88 888  888      888        d8b   888  " << endl;
+	cout << "    Y8P  d88P     888   Y8888P   Y888888.      888888888  Y8P 8888888\n" << endl;
+
+
+	cout << "*****************************************************************" << endl;
+	cout << "*  Inyector VACa 2.1 - www.CheatersUnidos.tk - 16 - Julio - 2020 *" << endl;
+	cout << "*****************************************************************\n\n" << endl;
+
+
+
+	cout << "Creadores y Agradecimientos Especiales para:\n Daniel Krupinski, online-9, Hitchance\n\n" << endl;
 
 	cout << "Traducido y Recompilado:\n SuperK2 & Gauxalot\n\n" << endl;
 
-	cout << "www.CheatersUnidos.tk - Discord: https://discord.gg/4Sh8PJP" << endl;
+	cout << "www.CheatersUnidos.tk - Discord: https://discord.gg/4Sh8PJP \n\n" << endl;
 
 	inj.hwndproc = FindWindowA(0, "Counter-Strike: Global Offensive");
 
@@ -75,28 +84,25 @@ int main()
 		bypass();
 
 		if (inj.inject(pid, "cheat.dll")) {
-
-			cout << "CHEAT INYECTADO CORRECTAMENTE, DISFRUTA ! GLHF !\n\n" << endl;
+			cout << "CHEAT INYECTADO CORRECTAMENTE, DISFRUTA ! GLHF !!\n\n" << endl;
+			Backup();
 			Sleep(5000);
-
 			exit(0);
 		}
 		else
 		{
-
-			cout << "ERROR ! La Inyeccion ha fallado! \n\n" << endl;
+			cout << "ERROR ! La Inyeccion ha fallado!\n\n" << endl;
+			Backup();
 			Sleep(5000);
-			exit(0);
-
+			exit(-1);
 		}
 	
 	}
 	else
-
+	{
 		cout << "EL FICHERO cheat.dll NO SE HA ENCONTRADO, RECUERDA RENOMBRAR TU .DLL como cheat.dll\n\n";
 		Sleep(5000);
-		exit(0);
-
+		exit(-1);
 	}
 
 	return 0;
